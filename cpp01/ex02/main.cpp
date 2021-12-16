629477897e7c79cc3258cec22f15809b9d5ff080
inyang <inyang@student.42seoul.kr>
2021-12-17 02:12:37 +0900

    ex03 ing

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: inyang <inyang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 16:28:48 by inyang            #+#    #+#             */
-/*   Updated: 2021/12/15 20:28:51 by inyang           ###   ########.fr       */
+/*   Updated: 2021/12/17 00:39:11 by inyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,13 @@ int	main(void)
 	std::string		str = "HI THIS IS BRAIN";
 	std::string		*ptr = &str;
 	std::string&	ref = str;
+	
+	std::cout << "string : " << str << std::endl;
+	std::cout << "string address: " << &str << std::endl;
+	std::cout << std::endl;
+	std::cout << "pointer: " << *ptr << std::endl;
+	std::cout << "pointer address: " << ptr << std::endl;
+	std::cout << std::endl;
+	std::cout << "reference: " << ref << std::endl;
+	std::cout << "reference address: " << &ref << std::endl;
 }
